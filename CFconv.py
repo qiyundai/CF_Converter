4c46604db2413e40061d75464c34374c6c3289b2
Qiyun Dai <pianoquin@gmail.com>
2020-01-02 00:13:27 -0600

    changing functionaility to one-question process.

--- CFconv.py
+++ CFconv.py
@@ -1,21 +1,13 @@
-def checksystem(letter):
-    if letter == "C":
-        degreec = input("Please type in the degree in C°: ")
-        convert_ctof(int(degreec))
-
-    elif letter == "F":
-        degreef = input("Please type in the degree in F°: ")
-        convert_ftoc(int(degreef))
-
-    else:
-        print("Error; please type uppercase C or F.")
+convertfrom = input("Please type in the degree, then follow with C or F: ")
 
 def convert_ctof(degf):
-    print((degf * 9/5) + 32)
+    print((degf * 9/5) + 32,"F°")
 
 def convert_ftoc(degc):
-    print((degc - 32) * 5/9)
+    print((degc - 32) * 5/9,"C°")
 
-system = input("Converting from Fahrenheit or Celsius? Please type C for Celsius or F for Fahrenheit. ")
-
-checksystem(system)
+if "C" in convertfrom:
+    convert_ctof(float(convertfrom[:-1]))
+if "F" in convertfrom:
+    convert_ftoc(float(convertfrom[:-1]))
+    
